9461b0b548e1be6a07b11666b06bb5bc088a174e
vplancak <vladimirplancak@gmail.com>
2024-07-24 18:53:03 +0200

    remove obsolete code

--- web-main/src/app/components/core/user/index.ts
+++
@@ -1,1 +0,0 @@
-export { TestComponent as Component } from './test'
--- web-main/src/app/components/core/user/test.ts
+++
@@ -1,9 +0,0 @@
-import * as ngCore from '@angular/core'
-
-@ngCore.Component({
-  template: '',
-  standalone: true
-})
-export class TestComponent {
-
-}
