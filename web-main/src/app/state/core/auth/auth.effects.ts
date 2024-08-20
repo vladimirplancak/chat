a2ae235b209431bebf615bb67d2124f25b817ca6
vplancak <vladimirplancak@gmail.com>
2024-08-20 18:40:20 +0200

    Adding TODO comments

--- web-main/src/app/state/core/auth/auth.effects.ts
+++ web-main/src/app/state/core/auth/auth.effects.ts
@@ -16,6 +16,10 @@ export class AuthEffects implements ngrxEffects.OnInitEffects{
   private readonly _actions = ngCore.inject(ngrxEffects.Actions)
   private readonly _authApiService = ngCore.inject(services.AuthApiService)
 
+  /**
+   * When the application loads, this effect will execute exactly once, and it
+   * will trigger / dispatch 'localAuthStarted' action.
+   */
   initialized$ = ngrxEffects.createEffect(() =>
     this._actions.pipe(
       ngrxEffects.ofType(actions.Auth.Misc.actions.initialized),
@@ -23,6 +27,11 @@ export class AuthEffects implements ngrxEffects.OnInitEffects{
     )
   )
 
+  /**
+   * When 'localAuthStarted' action is dispatched, we need to check if the token
+   * that we have "cached" in the localStorage is valid, and is ready to be
+   * used.
+   */
   localAuthStarted$ = ngrxEffects.createEffect(() => this._actions.pipe(
     ngrxEffects.ofType(actions.Auth.Misc.actions.localAuthStarted),
     rxjs.switchMap(() => this._authApiService.validateJwt()),
@@ -35,20 +44,36 @@ export class AuthEffects implements ngrxEffects.OnInitEffects{
     )
   ))
 
+  localAuthFailed$ = ngrxEffects.createEffect(() => this._actions.pipe(
+    ngrxEffects.ofType(actions.Auth.Misc.actions.localAuthFailed),
+    rxjs.map(() => {
+      // TODO: redirect to login page
+      // this._router.navigate(['/login'])
+      throw new Error('Local authentication failed')
+    })
+  ), { dispatch: false })
 
+
+  /**
+   * When the user submits the login form, we want to start the login process.
+   */
   onFormSubmitted$ = ngrxEffects.createEffect(() => this._actions.pipe(
     ngrxEffects.ofType(actions.Auth.Ui.LoginForm.actions.submitted),
     rxjs.map((action) => actions.Auth.Api.actions.started(action))
-   
   ))
 
-  onApiStarted$ = ngrxEffects.createEffect(() => this._actions.pipe(
+  /**
+   * Login process is started, we need to call the API to login and obtain the
+   * JWT token and do the standard (success, fail) actions.
+   */
+  onRemoteApiStarted$ = ngrxEffects.createEffect(() => this._actions.pipe(
     ngrxEffects.ofType(actions.Auth.Api.actions.started),
-    rxjs.switchMap((action) => this._authApiService.login(action.username, action.password).pipe(
+    rxjs.switchMap((action) => 
+      this._authApiService.login(action.username, action.password).pipe(
       rxjs.map(jwtToken => actions.Auth.Api.actions.succeeded({jwtToken})),
       rxjs.catchError(error =>
         rxjs.of(actions.Auth.Api.actions.failed({errorMessage: error?.message}))
-      ) 
+      )
     ))
   ))
 
