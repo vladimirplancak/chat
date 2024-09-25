89bce7d252403a3b1f2759c355a5dbedb330f887
vplancak <vladimirplancak@gmail.com>
2024-09-25 19:18:15 +0200

    Updating computed signal to new structure

--- web-main/src/app/components/pages/cons-page/components/cons-header/cons-header.component.ts
+++ web-main/src/app/components/pages/cons-page/components/cons-header/cons-header.component.ts
@@ -1,7 +1,6 @@
 import * as ngCore from '@angular/core';
 import * as ngrxStore from '@ngrx/store'
 import * as state from '../../../../../state'
-import * as models from '../../../../../models'
 
 @ngCore.Component({
   standalone: true,
@@ -11,13 +10,12 @@ import * as models from '../../../../../models'
 })
 export class ConsHeaderComponent {
   private readonly _store = ngCore.inject(ngrxStore.Store)
-  public readonly selfSg  = ngCore.computed(() => {
-    const lookup = this._store.selectSignal(state.core.user.selectors.User.USERS_LOOKUP)()
-    const selfId = this._store.selectSignal(state.core.auth.selectors.Auth.SELF_ID)()
-
-    return selfId 
-      ? lookup[selfId] 
+  public readonly selfSg = ngCore.computed((
+    lookup = this._store.selectSignal(state.core.user.selectors.User.USERS_LOOKUP)(),
+    selfId = this._store.selectSignal(state.core.auth.selectors.Auth.SELF_ID)()
+  ) => selfId
+      ? lookup[selfId]
       : undefined
-  })
-  
+  )
+
 }
